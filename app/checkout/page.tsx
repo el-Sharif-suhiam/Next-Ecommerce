b378b9b57c0e47ec351dd629eb27463e63b796e3
el-sharif-suhiam <zero_robot77@protonmail.com>
2024-11-01 23:10:02 +0200

    wrap searchParams in suspance

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,29 +1,31 @@
 // @ts-nocheck
-
 "use client";
-import React from "react";
+
+import React, { Suspense } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_component/CheckoutForm";
 import { useSearchParams } from "next/navigation";
+
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY as string
 );
-function page() {
+
+function Page() {
   const searchParams = useSearchParams();
   const options = {
-    // passing the client secret obtained from the server
-    //clientSecret: "{{CLIENT_SECRET}}",
     mode: "payment",
     currency: "usd",
     amount: Number(searchParams.get("amount")) * 100,
   };
 
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm amount={Number(searchParams.get("amount"))} />
-    </Elements>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm amount={Number(searchParams.get("amount"))} />
+      </Elements>
+    </Suspense>
   );
 }
 
-export default page;
+export default Page;
